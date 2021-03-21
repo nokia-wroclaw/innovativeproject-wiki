81f55cc4b9f6bd09794dd87cc7f807f7bf7c4194
radoslawlodzinski <radeklodzinski@gmail.com>
2021-03-21 22:15:28 +0100

    Add Router

--- frontend/src/components/Appbar/Appbar.tsx
+++ frontend/src/components/Appbar/Appbar.tsx
@@ -7,7 +7,7 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import useStyles from './Appbar.styles';
 
-export default function ButtonAppBar() {
+export default function Appbar() {
   const classes = useStyles();
 
   return (
---
+++ frontend/src/pages/About.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default function About() {
+  return (
+    <div>
+      <Link to="/">Go to Home</Link>
+    </div>
+  );
+}
