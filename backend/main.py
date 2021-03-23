a72204d41b51e2d8e26f3bf15596541a57b0d309
Workata <jakubtolsciuk@gmail.com>
2021-03-23 14:04:55 -0700

    fix-3

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,14 @@
+"""
+This is module docstring.
+Main module that runs basic fast api app.
+"""
 from fastapi import FastAPI
 
 app = FastAPI()
 
-
+"""
+This is function docstring.
+"""
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
