1fdf4b56d905f37177c016981568aa550a654707
Dominik Kurowski <d.kurowski98@gmail.com>
2021-03-27 16:37:37 +0100

    Add all docstrings Change Message 'value' field to int type

--- backend/user_db.py
+++ backend/user_db.py
@@ -7,20 +7,25 @@ FAILURE = 1
 
 
 class UserDB:
-    """ TODO """
+    """ 
+    Interface for communication with the user database.
+
+    Attributes:
+        db (TinyDB): instance of the database
+    """
 
     def __init__(self):
         self.db = TinyDB("db.json")
 
     def does_user_exist(self, username: str) -> bool:
         """
-        Checks if user with a given username already exists in the database.
+        Checks if a user with a given username already exists in the database.
 
         Parameters:
-            username (str): Given username
+            username (str): searched user
 
         Returns:
-            bool: True if user exists in a database, False otherwise
+            bool: True if a user exists in a database, False otherwise
         """
         if not _is_non_empty_string(username):
             return False
@@ -28,7 +33,17 @@ class UserDB:
         return self.db.contains(where('username') == username)
 
     def add_user(self, username: str, password_hash: str, email: str):
-        """ TODO """
+        """
+        Adds a new user to a database (only if a username isn't already taken).
+
+        Parameters:
+            username (str): username of the new user
+            password_hash (str): hashed password of the new user
+            email (str): email of the new user
+
+        Returns:
+            Message: the result of an attempted user addition (SUCCESS/FAILURE) with additional info
+        """
         if not _is_non_empty_string((username, password_hash, email)):
             return Message("ERROR", "Invalid parameter type detected", FAILURE)
 
@@ -43,7 +58,15 @@ class UserDB:
             return Message("INFO", "User added successfully", SUCCESS)
 
     def remove_user(self, username: str):
-        """ TODO """
+        """
+        Removes user from a database (only if user exists).
+
+        Parameters:
+            username (str): user to remove
+
+        Returns:
+            Message: the result of an attempted user deletion (SUCCESS/FAILURE) with some additional info
+        """
         if not _is_non_empty_string(username):
             return Message("ERROR", "Invalid parameter type detected", FAILURE)
 
@@ -54,7 +77,18 @@ class UserDB:
             return Message("INFO", "User removed successfully", SUCCESS)
 
     def get_user_data(self, username: str):
-        """ TODO """
+        """
+        Get user data from a database (only if user exists).
+
+        Parameters:
+            username (str): the user whose data we are interested in
+
+        Returns:
+        If extraction of the user data was successful:
+            dict: {"username": x, "password_hash": x, "email": x}
+        Otherwise:
+            Message: information about the cause of failure
+        """
         if not _is_non_empty_string(username):
             return Message("ERROR", "Invalid parameter type detected", FAILURE)
 
@@ -64,7 +98,17 @@ class UserDB:
             return Message("INFO", "Given user doesn't exists", FAILURE)
 
     def edit_user_data(self, username, edited_field, new_value):
-        """ TODO """
+        """
+        Removes user from a database (only if user exists).
+
+        Parameters:
+            username (str): user we are editing
+            edited_field (str): field we want to edit. Possible values: "username", "hashed_password", "email"
+            new_value (str): new value of selected field
+
+        Returns:
+            Message: the result of an attempted user data edition (SUCCESS/FAILURE) with some additional info
+        """
         if not _is_non_empty_string((username, new_value)):
             return Message("ERROR", "Invalid parameter type detected", FAILURE)
 
@@ -81,16 +125,17 @@ class UserDB:
 
 def _is_non_empty_string(data: list) -> bool:
     """
-    Checks if 
+    Checks if all values in list are non-empty string
 
     Parameters:
-        data (list): 
+        data (list): list of values ​​to be checked 
 
     Returns:
-        bool: 
+        bool: True if all values are non-empty string, False otherwise
     """
     for field in data:
         if not isinstance(field, str) or not field:
             return False
 
     return True
+    
