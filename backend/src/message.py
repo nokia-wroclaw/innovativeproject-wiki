5818e313667ea1cb363f06bdd5788ef3b121f93d
FilipJQ77 <filipprzygonski99@gmail.com>
2021-04-03 21:28:41 +0200

    add log to std function

--- backend/src/message.py
+++ backend/src/message.py
@@ -3,6 +3,7 @@
     This module is responsible for everything related with logs.
     TODO: Make this docstring better
 """
+import sys
 from datetime import datetime, date
 from typing import Optional
 
@@ -10,7 +11,7 @@ FILE = 0  # log to file
 STD = 1  # log to stdout or stderr
 SYSLOG = 2  # log to syslog
 
-log_type = FILE  # default for now; TODO: read from some config file
+LOG_TYPE = FILE  # default for now; TODO: read from some config file
 
 
 class Message:
@@ -44,7 +45,7 @@ class Message:
         self.value = value
 
 
-def log(message: Message) -> None:
+def log_to_file(message: Message) -> None:
     """
         Writes logs to files.
         Logs are stored in 'logs\' folder.
@@ -62,3 +63,23 @@ def log(message: Message) -> None:
         now = datetime.now()
         current_time = now.strftime('%H:%M:%S')
         file.write(f"[{message.category}] [{current_time}] [{message.value}] {message.content}\n")
+
+
+# todo improve?
+def log_to_std(message: Message, is_error: bool) -> None:
+    """
+        Writes logs to stdout/stderr depending on the message.
+
+        Parameters:
+            message (Message): Message that contains log information.
+            is_error (bool): todo write something
+
+        Returns:
+            None
+    """
+    now = datetime.now()
+    day_time = now.strftime('%Y_%m_%d %H:%M:%S')
+    if is_error:
+        print(f"[{message.category}] [{day_time}] [{message.value}] {message.content}\n", file=sys.stderr)
+    else:
+        print(f"[{message.category}] [{day_time}] [{message.value}] {message.content}\n")
