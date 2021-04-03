04834e901fa1ee1d6c37a6271c97ed0ddb96fdc6
FilipJQ77 <filipprzygonski99@gmail.com>
2021-04-03 21:35:30 +0200

    Add log to syslog and general log functions

--- backend/src/message.py
+++ backend/src/message.py
@@ -4,6 +4,7 @@
     TODO: Make this docstring better
 """
 import sys
+import syslog
 from datetime import datetime, date
 from typing import Optional
 
@@ -45,6 +46,24 @@ class Message:
         self.value = value
 
 
+def log(message: Message) -> None:
+    """
+        Writes logs to the destination given in config file.
+
+        Parameters:
+            message (Message): Message that contains log information.
+
+        Returns:
+            None
+    """
+    if LOG_TYPE == FILE:
+        log_to_file(message)
+    elif LOG_TYPE == STD:
+        log_to_std(message)
+    elif LOG_TYPE == SYSLOG:
+        log_to_syslog(message)
+
+
 def log_to_file(message: Message) -> None:
     """
         Writes logs to files.
@@ -65,8 +84,7 @@ def log_to_file(message: Message) -> None:
         file.write(f"[{message.category}] [{current_time}] [{message.value}] {message.content}\n")
 
 
-# todo improve?
-def log_to_std(message: Message, is_error: bool) -> None:
+def log_to_std(message: Message) -> None:
     """
         Writes logs to stdout/stderr depending on the message.
 
@@ -79,7 +97,21 @@ def log_to_std(message: Message, is_error: bool) -> None:
     """
     now = datetime.now()
     day_time = now.strftime('%Y_%m_%d %H:%M:%S')
-    if is_error:
+    if message.category == "ERROR":  # todo improve
         print(f"[{message.category}] [{day_time}] [{message.value}] {message.content}\n", file=sys.stderr)
     else:
         print(f"[{message.category}] [{day_time}] [{message.value}] {message.content}\n")
+
+
+# https://docs.python.org/3/library/syslog.html todo improve
+def log_to_syslog(message: Message) -> None:
+    """
+        Writes logs to syslog.
+
+        Parameters:
+            message (Message): Message that contains log information.
+
+        Returns:
+            None
+    """
+    syslog.syslog(message.content)
