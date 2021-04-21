f1a00cfae271e5c1e46c260e068fa3058a089245
Dominik Kurowski <d.kurowski98@gmail.com>
2021-04-21 22:23:01 +0200

    Add info.csv

--- backend/app/routers/workspace_manager.py
+++ backend/app/routers/workspace_manager.py
@@ -1,6 +1,7 @@
 """
 TODO module docstring
 """
+import csv
 import json
 from pathlib import Path
 from fastapi import APIRouter
@@ -15,7 +16,7 @@ DOCS_DIR = "docs"
 DOC_IMGS_DIR = "imgs"
 DOC_ATCHS_DIR = "atchs"
 
-INFO_FILE = "info.json"
+INFO_FILE = "info.csv"
 CONFIG_FILE = "config.json"
 
 
@@ -23,7 +24,7 @@ def get_workspace_path(workspace_id: str = ""):
     """
     TODO function docstring
     """
-    path = Path(".")/DATA_DIR/WORKSPACES_DIR/workspace_id
+    path = Path(".") / DATA_DIR / WORKSPACES_DIR / workspace_id
     return path if path.exists else None
 
 
@@ -35,7 +36,7 @@ def get_doc_path(workspace_id: str, doc_id: str = ""):
     if workspace_path is None:
         return None
 
-    path = workspace_path/DOCS_DIR/doc_id
+    path = workspace_path / DOCS_DIR / doc_id
     return path if path.exists else None
 
 
@@ -47,7 +48,7 @@ def get_doc_img_path(workspace_id: str, doc_id: str, img_id: str = ""):
     if doc_path is None:
         return None
 
-    path = doc_path/DOC_IMGS_DIR/img_id
+    path = doc_path / DOC_IMGS_DIR / img_id
     return path if path.exists else None
 
 
@@ -59,23 +60,45 @@ def get_doc_atch_path(workspace_id: str, doc_id: str, atch_id: str = ""):
     if doc_path is None:
         return None
 
-    path = doc_path/DOC_ATCHS_DIR/atch_id
+    path = doc_path / DOC_ATCHS_DIR / atch_id
     return path if path.exists else None
 
+
+def update_main_info_file(workspace_id: str, workspace_name: str, creator: str):
+    path = Path(".") / DATA_DIR / WORKSPACES_DIR / INFO_FILE
+    if not path.exists:
+        return {
+            "Type": "EROOR",
+            "Message": f"No such file: {path.absolute()}",
+        }
+
+    with open(path.absolute(), "a") as file:
+        writer = csv.writer(file)
+        writer.writerow([workspace_id, workspace_name, creator])
+
+    return {
+        "Type": "INFO",
+        "Message": "File updated successfuly",
+    }
+
+
 @router.post("/workspace/new/{workspace_name}", tags=["workspaces"])
-async def create_new_workspace(workspace_name: str, creator_username: str):
+async def create_new_workspace(workspace_name: str, creator: str):
     """
     TODO function docstring
     """
-    workspace_path = get_workspace_path() / random_filename()
+    workspace_id = random_filename()
+    workspace_path = get_workspace_path() / workspace_id
 
     workspace_path.mkdir()
     (workspace_path / DOCS_DIR).mkdir()
 
-    with open(workspace_path/INFO_FILE, 'w') as info:
-        info.write("")
+    update_main_info_file(workspace_id, workspace_name, creator)
 
-    with open(workspace_path/CONFIG_FILE, 'w') as config:
+    with open(workspace_path / CONFIG_FILE, "w") as config:
         config.write("")
 
-    return 0
+    return {
+        "Type": "INFO",
+        "Message": "Workspace created successfuly",
+    }
