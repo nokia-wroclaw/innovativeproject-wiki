8c5fdd3ee38d72b58aa3d8431151b2f4176aca68
Dominik Kurowski <45519705+d-kurowski@users.noreply.github.com>
2021-04-22 17:04:01 +0200

    Fix typos

--- backend/app/dependencies.py
+++ backend/app/dependencies.py
@@ -6,9 +6,9 @@ import string
 import secrets
 
 
-def random_filename(lenght=16, endwith=""):
+def random_filename(length=16, endwith=""):
     """
     TODO function docstring
     """
     alpha_num = string.ascii_letters + string.digits
-    return "".join(secrets.choice(alpha_num) for _ in range(lenght)) + endwith
+    return "".join(secrets.choice(alpha_num) for _ in range(length)) + endwith
