5a2165c8f387407d78975f012b7aa198795ba053
Fohin <fohinek99@gmail.com>
2021-04-23 13:48:16 +0200

    Create a prototype of the home page

--- frontend/src/pages/Home/Home.styles.ts
+++ frontend/src/pages/Home/Home.styles.ts
@@ -1,11 +1,32 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
-  loginPaper: {
+  homeTitlePaper: {
     padding: 40,
-    height: 350,
-    width: 1248,
+    height: 200,
+    width: 800,
     margin: '30px auto',
+    fontSize: 180,
+  },
+  firstFeaturePaper: {
+    padding: 40,
+    height: 30,
+    width: 350,
+    margin: '30px auto',
+    fontWeight: 'bold',
+    fontSize: 40,
+  },
+  firstFeature: {
+    color: '#0386D0',
+    fontSize: 40,
+  },
+  secondFeature: {
+    color: 'red',
+    fontSize: 40,
+  },
+  thirdFeature: {
+    color: 'grey',
+    fontSize: 40,
   },
 }));
 
--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Button, Grid, Paper } from '@material-ui/core';
+import CreateIcon from '@material-ui/icons/Create';
+import SettingsIcon from '@material-ui/icons/Settings';
+import SearchIcon from '@material-ui/icons/Search';
 import useStyles from './Home.styles';
 
 export default function Home() {
@@ -11,20 +14,44 @@ export default function Home() {
       <Button color="primary" to="/editor"component={Link}>
         Go to Editor Page
       </Button>
-      <Paper elevation={10} className={classes.loginPaper}>
+      <Paper elevation={10} className={classes.homeTitlePaper}>
         <Grid
           container
           direction="row"
           justify="space-around"
           alignItems="center"
-        >
-        <div style={{fontSize: 180, marginTop: 70}} >
-          InnoDocs
-        </div>
-               
-        
+        > 
+          InnoDocs        
         </Grid>
       </Paper>
+
+      <Grid 
+        container
+        direction="row"
+        justify="space-around"
+        alignItems="center"
+        >
+        <Grid> 
+          <Paper elevation={5} square={false} className={classes.firstFeaturePaper}>
+            <CreateIcon className={classes.firstFeature}/>
+              EASY-TO-USE
+          </Paper>
+        </Grid>
+        <Grid>
+          <Paper elevation={5} className={classes.firstFeaturePaper}>
+            <SearchIcon className={classes.secondFeature}/>
+              SEARCHEABLE
+          </Paper>
+        </Grid>
+        <Grid>
+          <Paper elevation={5} className={classes.firstFeaturePaper}>
+            <SettingsIcon className={classes.thirdFeature}/>
+              CONFIGURABLE
+          </Paper>
+        </Grid>
+      </Grid>
     </div>
+    
+    
   );
 }
