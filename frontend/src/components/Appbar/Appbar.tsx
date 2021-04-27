9a43f2e3d866b3a5c7202034aaa836fced3e02a6
radoslawlodzinski <radeklodzinski@gmail.com>
2021-04-27 18:55:48 +0200

    Title to left

--- frontend/src/components/Appbar/Appbar.tsx
+++ frontend/src/components/Appbar/Appbar.tsx
@@ -3,8 +3,7 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
+import Container from '@material-ui/core/Container';
 import { Link } from 'react-router-dom';
 import useStyles from './Appbar.styles';
 
@@ -15,14 +14,6 @@ export default function Appbar() {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-          >
-            <MenuIcon />
-          </IconButton>
           <Typography
             variant="h6"
             className={classes.title}
@@ -31,12 +22,14 @@ export default function Appbar() {
           >
             InnoDocs
           </Typography>
-          <Button color="inherit" to="/login" component={Link}>
-            Login
-          </Button>
-          <Button color="inherit" to="/register" component={Link}>
-            Register
-          </Button>
+          <Container className={classes.buttons}>
+            <Button color="inherit" to="/login" component={Link}>
+              Login
+            </Button>
+            <Button color="inherit" to="/register" component={Link}>
+              Register
+            </Button>
+          </Container>
         </Toolbar>
       </AppBar>
     </div>
