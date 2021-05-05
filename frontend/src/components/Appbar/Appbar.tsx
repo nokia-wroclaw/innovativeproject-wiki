271992e3637d953546ba651717b8631630483a9a
Workata <jakubtolsciuk@gmail.com>
2021-05-05 08:17:53 -0700

    Align appbar

--- frontend/src/components/Appbar/Appbar.tsx
+++ frontend/src/components/Appbar/Appbar.tsx
@@ -28,77 +28,58 @@ export default function Appbar() {
   if (token ===  '') // ! workaround for testing purpose - change this for proper user validation (use Context)
   {
   return (
-      <AppBar position="static">
-        <Toolbar>
-        <div className={classes.root}>
-          <Typography
-            variant="h6"
-            className={classes.title}
-            to="/"
-            component={Link}
-            align="left"
-          >
-            InnoDocs
-          </Typography>
-          <Button
-            color="inherit"
-            to="/editor"
-            component={Link}
-            style={{ marginLeft: 100 }}
-          >
-            Editor
+    <AppBar position="static">
+    <Toolbar>
+      <div className={classes.root}>
+        <Typography
+          variant="h6"
+          className={classes.title}
+          to="/"
+          component={Link}
+          align="left"
+        >
+          InnoDocs
+        </Typography>
+        <div className={classes.buttons}>
+          <Button color="inherit" to="/login" component={Link}>
+            Login
           </Button>
-          <Container className={classes.buttons}>
-              <Button color="inherit" to="/login" component={Link}>
-                Login
-              </Button>
-              <Button color="inherit" to="/register" component={Link}>
-                Register
-              </Button>
-          </Container>
-          </div>
-        </Toolbar>
-      </AppBar>
+          <Button color="inherit" to="/register" component={Link}>
+            Register
+          </Button>
+        </div>
+      </div>
+    </Toolbar>
+  </AppBar>
   );
   }
   
   // console.log("Token:".concat(token).concat("End"))
 
   return (
+
     <AppBar position="static">
-      <Toolbar>
-        <div className={classes.root}>
-          <Typography
-            variant="h6"
-            className={classes.title}
-            to="/"
-            component={Link}
-            align="left"
-          >
-            InnoDocs
-          </Typography>
-          <Button
-            color="inherit"
-            to="/editor"
-            component={Link}
-            style={{ marginLeft: 100 }}
-          >
-            Editor
-          </Button>
-
-          <Container className={classes.buttons}>
-
+    <Toolbar>
+      <div className={classes.root}>
+        <Typography
+          variant="h6"
+          className={classes.title}
+          to="/"
+          component={Link}
+          align="left"
+        >
+          InnoDocs
+        </Typography>
+        <div className={classes.buttons}>
             <Button color="inherit" to="/userData" component={Link}>
-                  Profile
+              Profile
             </Button>
-
             <Button color="inherit" to="/" component={Link} onClick={() => {document.cookie = "token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;"; }}>
                   Logout
             </Button>
-
-          </Container>
-          </div>
-        </Toolbar>
-      </AppBar>
+        </div>
+      </div>
+    </Toolbar>
+  </AppBar>
   );
 }
