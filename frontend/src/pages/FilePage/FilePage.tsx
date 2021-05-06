10a61a8a42c4a1b932f71f13549a86b395e405c5
Fedi <pawelfedorczak98@gmail.com>
2021-05-06 12:17:37 +0200

    Add export button, minor visual changes

--- frontend/src/pages/FilePage/FilePage.tsx
+++ frontend/src/pages/FilePage/FilePage.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import CloudDownloadIcon from '@material-ui/icons/CloudDownload';
 import Sidebar from '../../components/Sidebar/Sidebar';
 import TextEditor from '../../components/TextEditor/TextEditor';
-
+import useStyles from './FilePage.styles';
+/*
 const useStyles = makeStyles((theme) => ({
   editPageContainer: {
     display: 'flex',
@@ -17,7 +19,7 @@ const useStyles = makeStyles((theme) => ({
   editor: {
     margin: 40,
   },
-}));
+}));  */
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default function FilePage({ match }: { match: any }) {
@@ -25,13 +27,24 @@ export default function FilePage({ match }: { match: any }) {
   const { fileName } = match.params;
 
   return (
-    <div className={classes.editPageContainer}>
+    <div className={classes.filePageContainer}>
       <div className={classes.sidebar}>
         <Sidebar />
       </div>
       <div className={classes.editor}>
         <TextEditor fileName={fileName} />
       </div>
+
+      <div className={classes.filePage_buttons}>
+        <Button
+          variant="contained"
+          color="primary"
+          className={classes.filePage_buttons}
+          startIcon={<CloudDownloadIcon/>}
+        >
+          Export
+        </Button>
+        </div>
     </div>
   );
 }
